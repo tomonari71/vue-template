a851583716c5991e045c958051220e7211fe2632
t.okada <tomonari.okada@gmail.com>
2018-04-09 02:09:47 +0900

    'pug、sass、axios導入済み。stylelint設定途中'

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template src="./App.pug" lang="pug"></template>
+
+<script src="./App.js"></script>
+
+<style src="./App.scss" lang="scss"></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template src="./HelloWorld.pug" lang="pug"></template>
+
+<script src="./HelloWorld.js"></script>
+
+<style src="./HelloWorld.scss" lang="scss" scoped></style>
